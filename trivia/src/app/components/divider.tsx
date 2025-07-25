f78808880fbc5db64777314805c384bdcfc8bd22
Collin Giess <giesscollin@gmail.com>
2025-07-25 00:53:37 -0400

    add divider color

--- trivia/src/app/components/divider.tsx
+++ trivia/src/app/components/divider.tsx
@@ -1,3 +1,5 @@
 export function Divider({ className }: { className?: string }) {
-  return <hr className={`border-t border-gray-300 ${className}`} />;
+  return (
+    <hr className={`border-t border-divider brightness-150 ${className}`} />
+  );
 }
