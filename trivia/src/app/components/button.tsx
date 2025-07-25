3628ea6e85ce1c9ae683c628817c08ebd37cd7f8
Collin Giess <giesscollin@gmail.com>
2025-07-24 23:49:37 -0400

    add basic button component

---
+++ trivia/src/app/components/button.tsx
@@ -0,0 +1,27 @@
+type ButtonVariant = 'primary' | 'secondary' | 'outlined';
+
+export interface ButtonProps {
+  variant?: ButtonVariant;
+  children: React.ReactNode;
+}
+
+export function Button({ variant = 'primary', children }: ButtonProps) {
+  function variantStyles(): string {
+    switch (variant) {
+      case 'secondary':
+        return 'bg-gray-500 text-white hover:bg-gray-600';
+      case 'outlined':
+        return 'border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white';
+      default:
+        return 'bg-blue-500 text-white hover:bg-blue-600';
+    }
+  }
+
+  return (
+    <button
+      className={`cursor-pointer font-bold py-2 px-4 rounded transition duration-100 ease-in-out ${variantStyles()}`}
+    >
+      {children}
+    </button>
+  );
+}
